7956423f9771dac64bdbf357373176db92a7756b
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2022-04-08 23:38:03 +0100

    First Commit

---
+++ travel-journal/src/components/styles.js
@@ -0,0 +1,52 @@
+import styled from 'styled-components'
+
+export const AppSection = styled.section`
+  margin: auto;
+  width: 75vw;
+` 
+export const HeaderContainer = styled.header`
+  background-color: #F55A5A;
+  color: white;
+  text-align: center;
+  padding: 10px 0;
+  font-style: normal;
+  font-size: 12px;
+  line-height: 16px;
+  letter-spacing: -0.075em;
+  margin-top: 20px;
+`
+export const CardContainer = styled.section`
+  display: flex;
+  width: 90%;
+  margin: 15px auto;
+`
+export const InfoContainer = styled.div`
+  padding: 10px 0;
+  margin-left: 10px;
+  padding-left: 10px;
+  width: 100%;
+  color: #2B283A;
+`
+export const RegionSpan = styled.span`
+  /* letter-spacing: 1.5px; */
+  letter-spacing: 0.17em;
+  margin-left: 5px;
+`
+export const MapLink = styled.a`
+  margin-left: 5%;
+  color: #918E9B;
+`
+export const LocationName = styled.h2`
+  font-weight: 700;
+  font-size: 25px;
+  line-height: 30px;
+  margin-top: 5px; 
+  margin-bottom: 20px;
+`
+export const DateRange = styled.p`
+  font-weight: 700;
+  font-size: 12px;
+  line-height: 12px;
+  margin-bottom: 10px;
+` 
+
---
+++ travel-journal/src/dummyData.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    id: 1,
+    image: 'mountFuji.png',
+    region: 'JAPAN',
+    googleMapLink: '',
+    location: 'Mount Fuji',
+    dateRange: '12 Jan, 2021 - 24 Jan, 2021',
+    details: 'Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.',
+  },
+  {
+    id: 2,
+    image: 'sydneyOperaHouse.png',
+    region: 'AUSTRALIA',
+    googleMapLink: '',
+    location: 'Sydney Opera House',
+    dateRange: '27 May, 2021 - 8 Jun, 2021',
+    details: 'The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century\'s most famous and distinctive buildings.',
+  },
+  {
+    id: 3,
+    image: 'geirangerfjord.png',
+    region: 'NORWAY',
+    googleMapLink: '',
+    location: 'Geirangerfjord',
+    dateRange: '01 Oct, 2021 - 18 Nov, 2021',
+    details: 'The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.',
+  }
+]
